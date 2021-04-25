beb3b8ff02d4ce0e86d2930fabb3885554b2ee54
NOUAOURI Abdelkarim <57526755+abdellmum@users.noreply.github.com>
2021-04-25 21:14:48 +0000

    portfolio front end

---
+++ portfolio/src/App.vue
@@ -0,0 +1,424 @@
+<template>
+    <div class="vue-tempalte">
+      <div class="header">
+        <h1 class="tittle">YOUR NAME</h1>
+        <hr />
+        <h3 class="description">full stack devlopper</h3>
+      </div>
+      <div class=" col-12 about">
+        <h1>ABOUT ME</h1>
+        <p>Hope to know you after</p>
+        <hr />
+        <img class="profil" src="~@/assets/photo.jpg" alt="" />
+        <div class="row infos">
+          <div class="col-8 left">
+            <h3 class="text-left">WHO AM I</h3>
+            <p class=" lead text-success">
+              hello im here Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Aliquid repudiandae quod ut placeat, iusto repellendus harum
+              optio praesentium aut nulla neque, reiciendis nostrum id
+              voluptatibus veniam obcaecati omnis quae adipisci exercitationem
+              error fuga.
+            </p>
+            <p class="lead">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex
+              voluptas dicta cupiditate. Officia nostrum nulla corporis illo eos
+              ipsum illum laboriosam aliquam officiis culpa libero, omnis, non
+              numquam distinctio perspiciatis tempora dignissimos! A, temporibus
+              aut?
+            </p>
+          </div>
+          <div class="col-4 right">
+            <h3 class="text-left">NOUAOURI Karim</h3>
+            <h5>E-mail:<small> A.Nouaouri@student.youcode.ma</small></h5>
+            <h5>Phone:<small> +212626565101</small></h5>
+            <h5>City:<small> Youssoufia,Morocco</small></h5>
+            <h5>Language:<small>French,English,deutsh</small></h5>
+          </div>
+          <div class="experience">
+            <div class="experience__tittle">
+              <h1 class=" col-12 text-center ">EXPERIENCE</h1>
+              <h6>PROFESSIONAL AND ACADEMIC</h6>
+              <hr />
+            </div>
+            <div class="row carriere">
+              <div class="col-5 text-right  education">
+                <h3 class="cariere__tittle">EDUCATION</h3>
+                <div class="section">
+                  <h5>From octobre 2019</h5>
+                  <h6>YOUCODE</h6>
+                  <p>web developemnt formation</p>
+                </div>
+                <div class="section">
+                  <h5>From octobre 2019</h5>
+                  <h6>YOUCODE</h6>
+                  <p>web developemnt formation</p>
+                </div>
+               <div class="section">
+                  <h5>From octobre 2019</h5>
+                  <h6>YOUCODE</h6>
+                  <p>web developemnt formation</p>
+                </div>
+              </div>
+              <span class="vertical"></span>
+
+              <div class="col-5 text-left professional">
+                <h3 class="cariere__tittle">PROFESSIONAL</h3>
+                 <div class="section">
+                  <h5>From octobre 2019</h5>
+                  <h6>YOUCODE</h6>
+                  <p>web developemnt formation</p>
+                </div>
+               <div class="section">
+                  <h5>From octobre 2019</h5>
+                  <h6>YOUCODE</h6>
+                  <p>web developemnt formation</p>
+                </div>
+               <div class="section">
+                  <h5>From octobre 2019</h5>
+                  <h6>YOUCODE</h6>
+                  <p>web developemnt formation</p>
+                </div>
+              
+                
+               
+              </div>
+            </div>
+          </div>
+          <div class="skills">
+            <div class="skills__tittle">
+              <h3>SKILLS</h3>
+              <h6>HERE IS MY ARMAMENT</h6>
+               <hr />
+            </div>
+            <div class="row skills__img">
+              <div class="col-2">
+                <img class="technology" src="~@/assets/c.jpg" alt="" />
+                <h6>C</h6>
+                 
+              </div>
+              <div class="col-2">
+             <img class="technology" src="~@/assets/cplus.jpg" alt="" />
+             <h6>C++</h6>
+              </div>
+              <div class="col-2">
+              <img class="technology" src="~@/assets/csharp.jpg" alt="" />
+              <h6>C#</h6>
+              </div>
+              <div class="col-2">
+                 <img class="technology" src="~@/assets/java.jpg" alt="" />
+                 <h6>JAVA</h6>
+              </div>
+              <div class="col-2">
+                 <img class="technology" src="~@/assets/unity.jpg" alt="" />
+                 <h6>UNITY</h6>
+              </div>
+              <div class="col-2">
+                    <img class="technology" src="~@/assets/html.jpg" alt="" />
+                    <h6>HTML</h6>
+              
+              </div>
+            </div>
+               <div class="row skills__img">
+              <div class="col-2">
+                <img class="technology" src="~@/assets/css.jpg" alt="" />
+                <h6>CSS</h6>
+                 
+              </div>
+              <div class="col-2">
+             <img class="technology" src="~@/assets/php.jpg" alt="" />
+             <h6>PHP</h6>
+              </div>
+              <div class="col-2">
+              <img class="technology" src="~@/assets/laravel.jpg" alt="" />
+              <h6>LARAVEL</h6>
+              </div>
+              <div class="col-2">
+                 <img class="technology" src="~@/assets/vuejs.jpg" alt="" />
+                 <h6>VUE.JS</h6>
+              </div>
+              <div class="col-2">
+                 <img class="technology" src="~@/assets/android.jpg" alt="" />
+                 <h6>ANDROID</h6>
+              </div>
+              <div class="col-2">
+                    <img class="technology" src="~@/assets/linux.jpg" alt="" />
+                    <h6>LINUX</h6>
+              
+              </div>
+            </div>
+            <div class="row skills__img">
+              <div class="col-4">
+                <img class="technology" src="~@/assets/octocat.jpg" alt="" />
+                <h6>GIT/GITHUB</h6>  
+              </div>
+              <div class="col-4">
+             <img class="technology" src="~@/assets/go.jpg" alt="" />
+             <h6>GO</h6>
+              </div>
+              <div class="col-4">
+              <img class="technology" src="~@/assets/ovh.jpg" alt="" />
+              <h6>VPS/WEB HOSTING</h6>
+              </div>
+             
+            </div>
+          </div>
+          <!-- fin icons -->
+          <!-- debut my projects -->
+          <div class="projects">
+            <div class="projects__tittle">
+              <h3>MY PROJECTS</h3>
+              <h6>THIS IS MY STORY</h6>
+               <hr />
+
+            </div>
+            <div class="row projects__row">
+            <div class="col-4 esta">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis minima nesciunt ipsam hic. Id suscipit, fugit, voluptatum iste praesentium facilis sapiente porro accusamus deleniti eius, adipisci fugiat dolor quaerat soluta debitis minus asperiores quasi quia dolorem similique aliquam. Autem nihil, dignissimos quas commodi quia a consequuntur facilis ipsum nostrum. Vitae fugit a quasi quibusdam perferendis? At omnis illum sapiente deleniti quis nisi sit aut, necessitatibus earum dignissimos molestias quo magnam praesentium iusto cupiditate, atque explicabo ea perspiciatis, ducimus fuga possimus asperiores exercitationem dolore ipsa. Hic maxime aut recusandae libero?</p>
+            </div>
+            <div class="verticalLine">
+             <div class="arrow"><i class="fa fa-twitter"></i></div>
+            
+             <span class="vertical__projects"></span>
+             </div>
+             
+               <div class="col-7 esto">
+                
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nam autem voluptate repudiandae minima quod! Qui quidem error quos expedita, voluptatem similique aperiam veniam perferendis nisi tenetur aut impedit obcaecati optio, nam et magni esse saepe aliquam quo reprehenderit minima eius vel ad itaque? Cum.</p>
+               </div>
+               </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+html {
+  width: 100vw;
+   margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+.header {
+  background-image: url("~@/assets/bg.jpg");
+  background-position: center;
+  background-attachment: fixed;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: aliceblue;
+}
+hr {
+  background: aliceblue;
+  width: 35%;
+  height: 2px;
+  margin: 0 auto;
+}
+.tittle {
+  margin-bottom: 2rem;
+}
+.description {
+  margin-top: 2rem;
+}
+.left {
+}
+.right {
+}
+.infos {
+  width: 100vw;
+  margin: 0 auto;
+}
+
+
+.experience {
+  width: 100vw;
+  margin: 0 auto;
+}
+
+.experience__tittle {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+}
+.experience hr {
+  width: 12rem;
+  height: 0.3px;
+  background-color: white;
+  margin: 2rem auto;
+}
+.vertical {
+  border-right: 2px solid white;
+  display: inline-block;
+  height: 20rem;
+  margin: 0;
+}
+.education {
+  margin: 0 auto;
+  height: 20rem;
+ 
+}
+.professional {
+  margin: 0 auto;
+  height: 20rem;
+
+}
+.carriere {
+  width: 100vw;
+  margin: 0 auto;
+  background-color:rgb(88, 86, 86);
+  color: white ! important;
+}
+.about {
+  width: 100%;
+  background-color: #edebf1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: black;
+  align-items: center;
+ margin-left: 10rem;
+  overflow: hidden;
+}
+.cariere__tittle{
+  margin: 1rem auto;
+}
+.skills__img{
+  width: 80vw;
+  margin: 4rem auto !important;
+ 
+
+}
+.skills{
+  text-align: center;
+  width: 100vw;
+  margin: 2rem auto;
+
+}
+.skills__tittle hr{
+  width: 12rem;
+  height: 0.3px;
+background-color:rgb(88, 86, 86);
+  margin: 1rem auto;
+  
+}
+/* projects */
+.projects{
+  width: 100vw;
+  background-color:rgb(88, 86, 86);
+  height: 30rem;
+
+  
+}
+.projects__tittle{
+  width: 100vw;
+  text-align: center;
+  color: white;
+  background-color:rgb(88, 86, 86);
+  margin: 2rem auto;
+}
+.projects__tittle hr{
+   width: 12rem;
+  height: 0.3px;
+  background-color: white;
+  margin: 2rem auto;
+
+}
+
+
+.projects__row{
+  width: 100vw;
+  margin: 0 auto;
+}
+.vertical__projects {
+  border-right: 2px solid rgb(9, 58, 61);
+  display: inline-block;
+  height: 20rem;
+  margin: 0 auto;
+}
+.arrow{
+  width: 2rem;
+  height: 2rem;
+  border:1px solid tomato;
+ border-radius: 50%;
+margin: 0 auto;
+display: flex;
+align-items: center;
+justify-content: center;
+
+ 
+ 
+}
+.verticalLine{
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</style>
---
+++ portfolio/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vue-tempalte">
+    
+      <h1>your name</h1>
+      <hr>
+     <h3> full stack devlopper </h3>
+
+  </div>
+</template>
+
+<script>
+
+
+</script>
+<style scoped>
